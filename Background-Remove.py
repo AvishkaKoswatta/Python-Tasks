ecd77d73a2b3a8441cf27e9fc4b7de472dd249f1
Avishka <avishkakkoswaththa@gmail.com>
2025-04-09 23:14:48 +0530

    remove background

---
+++ Background-Remove.py
@@ -0,0 +1,18 @@
+from rembg import remove
+from PIL import Image
+
+# Input and output file paths
+input_path = 'image.jpg'
+output_path = 'image.png'
+
+# Open the input image
+inp = Image.open(input_path)
+
+# Remove the background
+output = remove(inp)
+
+# Save the output image
+output.save(output_path)
+
+# Open and show the saved image (optional)
+Image.open(output_path).show()
